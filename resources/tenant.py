73c9305241ecee8f244c40b53a50f24d2f27ef17
Bram Oosterlynck <booster@inuits.eu>
2021-04-21 16:46:26 +0200

    Fixes #75316 fix bugs brought up by tests

--- resources/tenant.py
+++ resources/tenant.py
@@ -65,6 +65,7 @@ class TenantDetail(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def patch(self, id):
+        self.abort_if_item_doesnt_exist("tenants", id)
         request = self.get_request_body()
         asset = self.storage.patch_item_from_collection("tenants", id, request)
         return asset, 201
@@ -92,6 +93,7 @@ class TenantDetail(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def put(self, id):
+        self.abort_if_item_doesnt_exist("tenants", id)
         request = self.get_request_body()
         abort_if_not_valid_tenant(request)
         tenant = self.storage.update_item_from_collection("tenants", id, request)
@@ -109,6 +111,6 @@ class TenantDetail(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def delete(self, id):
-        tenant = self.abort_if_item_doesnt_exist("tenants", id)
+        self.abort_if_item_doesnt_exist("tenants", id)
         self.storage.delete_item_from_collection("tenants", id)
         return "", 204
