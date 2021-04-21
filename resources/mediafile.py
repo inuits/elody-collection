2098b7a0ef70b0699bf4edd773f392138cbe2043
Bram Oosterlynck <booster@inuits.eu>
2021-04-21 14:38:24 +0200

    Refs #75316 fix bugs brought up by tests

--- resources/mediafile.py
+++ resources/mediafile.py
@@ -55,6 +55,7 @@ class MediafileDetail(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def patch(self, id):
+        self.abort_if_item_doesnt_exist("mediafiles", id)
         request = self.get_request_body()
         asset = self.storage.patch_item_from_collection("mediafiles", id, request)
         return asset, 201
@@ -82,6 +83,7 @@ class MediafileDetail(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def put(self, id):
+        self.abort_if_item_doesnt_exist("mediafiles", id)
         request = self.get_request_body()
         mediafile = self.storage.update_item_from_collection("mediafiles", id, request)
         return mediafile, 201
@@ -98,6 +100,6 @@ class MediafileDetail(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def delete(self, id):
-        mediafile = self.abort_if_item_doesnt_exist("mediafiles", id)
+        self.abort_if_item_doesnt_exist("mediafiles", id)
         self.storage.delete_item_from_collection("mediafiles", id)
-        return 204
+        return "", 204
