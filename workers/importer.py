b495e6e66a06ff5cc6d74b46ee37507c5a2f6254
Gijs Vermeulen <gverm@inuits.eu>
2021-07-08 11:22:22 +0200

    Use existing UPLOAD_FOLDER env variable

--- workers/importer.py
+++ workers/importer.py
@@ -7,7 +7,7 @@ import requests
 class Importer:
     def __init__(self, storage):
         self.storage_api_url = os.getenv("STORAGE_API_URL", "http://localhost:8001")
-        self.mount_point = os.getenv("MOUNT_POINT", "")
+        self.mount_point = os.getenv("UPLOAD_FOLDER", "")
         self.storage = storage
 
     def import_from_csv(self, path):
