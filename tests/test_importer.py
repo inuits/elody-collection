9dcccea5075022b24404d908a815a87eda2977b9
Gijs Vermeulen <gverm@inuits.eu>
2021-07-13 10:17:17 +0200

    Refs #79701 refactor tests & add tests for error cases

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -7,12 +7,31 @@ from tests.base_case import BaseCase
 class ImporterTest(BaseCase):
     upload_folder = os.getenv("UPLOAD_FOLDER", "/mnt/media-import")
 
-    def test_get_directories(self):
-        response = self.app.get(
-            "/importer/directories",
+    def import_csv(self, path):
+        upload_data = json.dumps(
+            {
+                "upload_folder": path,
+            }
+        )
+        response = self.app.post(
+            "/importer/start",
             headers={"Content-Type": "application/json"},
+            data=upload_data,
+        )
+        self.assertEqual(
+            response.json["data"]["upload_folder"],
+            os.path.join(self.upload_folder, path),
         )
 
+    def get_from_db(self, endpoint):
+        response = self.app.get(
+            endpoint,
+            headers={"Content-Type": "application/json"},
+        )
+        return response
+
+    def test_get_directories(self):
+        response = self.get_from_db("/importer/directories")
         self.assertEqual(list, type(response.json))
         self.assertEqual(
             response.json,
@@ -22,20 +41,16 @@ class ImporterTest(BaseCase):
             ],
         )
 
-    def test_start_import_no_csv(self):
-        upload_data = json.dumps(
-            {
-                "upload_folder": "Empty",
-            }
-        )
+    def test_import_no_csv(self):
+        self.import_csv("Empty")
+        response = self.get_from_db("/entities")
+        self.assertEqual(200, response.status_code)
+        self.assertFalse(response.json["count"])
+        self.assertFalse(response.json["results"])
 
-        response = self.app.post(
-            "/importer/start",
-            headers={"Content-Type": "application/json"},
-            data=upload_data,
-        )
-
-        self.assertEqual(
-            response.json["data"]["upload_folder"],
-            os.path.join(self.upload_folder, "Empty"),
-        )
+    def test_import_malformed_csv(self):
+        self.import_csv("Industrie Museum")
+        response = self.get_from_db("/entities")
+        self.assertEqual(200, response.status_code)
+        self.assertFalse(response.json["count"])
+        self.assertFalse(response.json["results"])
