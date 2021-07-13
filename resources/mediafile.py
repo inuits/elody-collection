45feae0d64cc31999265bf477d986061210bfa78
Gijs Vermeulen <gverm@inuits.eu>
2021-07-13 13:22:29 +0200

    Expand /mediafiles endpoint to return all mediafiles when receiving a GET request

--- resources/mediafile.py
+++ resources/mediafile.py
@@ -1,8 +1,7 @@
-from flask_restful import Resource
-from resources.base_resource import BaseResource
-from models.mediafile import MediafileModel
-
 import app
+
+from flask import request
+from resources.base_resource import BaseResource
 
 
 class Mediafile(BaseResource):
@@ -13,6 +12,25 @@ class Mediafile(BaseResource):
         request = self.get_request_body()
         mediafile = self.storage.save_item_to_collection("mediafiles", request)
         return mediafile, 201
+
+    @app.oidc.accept_token(
+        require_token=BaseResource.token_required, scopes_required=["openid"]
+    )
+    def get(self):
+        skip = int(request.args.get("skip", 0))
+        limit = int(request.args.get("limit", 20))
+        mediafiles = self.storage.get_items_from_collection("mediafiles", skip, limit)
+        count = mediafiles["count"]
+        mediafiles["limit"] = limit
+        if skip + limit < count:
+            mediafiles["next"] = "/{}?skip={}&limit={}".format(
+                "entities", skip + limit, limit
+            )
+        if skip > 0:
+            mediafiles["previous"] = "/{}?skip={}&limit={}".format(
+                "mediafiles", max(0, skip - limit), limit
+            )
+        return mediafiles
 
 
 class MediafileDetail(BaseResource):
