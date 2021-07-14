491aae26b76cf7965c6787c7dc3d54dd2b42b417
Gijs Vermeulen <gverm@inuits.eu>
2021-07-14 13:29:00 +0200

    Refactor and clean up memorystore some more

--- storage/memorystore.py
+++ storage/memorystore.py
@@ -13,116 +13,96 @@ class MemoryStorageManager:
         ]
         return items
 
-    def get_item_from_collection_by_id(self, collection, id):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        if main_id:
-            return self.collections[collection].get(main_id, None)
+    def get_item_from_collection_by_id(self, collection, obj_id):
+        if gen_id := self._get_collection_item_gen_id_by_identifier(collection, obj_id):
+            return self.collections[collection].get(gen_id, None)
         return None
 
-    def get_collection_item_metadata(self, collection, id):
-        collection_item = self.get_item_from_collection_by_id(collection, id)
-        if collection_item and "metadata" in collection_item:
-            return collection_item["metadata"]
+    def get_collection_item_metadata(self, collection, obj_id):
+        if item := self.get_item_from_collection_by_id(collection, obj_id):
+            return item["metadata"] if "metadata" in item else []
         return None
 
-    def get_collection_item_metadata_key(self, collection, id, key):
-        if self.get_item_from_collection_by_id(collection, id):
+    def get_collection_item_metadata_key(self, collection, obj_id, key):
+        if metadata := self.get_collection_item_metadata(collection, obj_id):
             return list(
                 filter(
                     lambda elem: elem["key"] == key,
-                    self.get_collection_item_metadata(collection, id),
+                    metadata,
                 )
             )
         return None
 
-    def get_collection_item_mediafiles(self, collection, id):
-        if self.get_item_from_collection_by_id(collection, id):
+    def get_collection_item_mediafiles(self, collection, obj_id):
+        if gen_id := self._get_collection_item_gen_id_by_identifier(collection, obj_id):
             return list(
                 filter(
-                    lambda elem: id in elem[collection],
+                    lambda elem: gen_id in elem[collection],
                     self.collections["mediafiles"].values(),
                 )
             )
         return None
 
-    def add_collection_item_metadata(self, collection, id, content):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        if main_id:
-            self.collections[collection][main_id]["metadata"].append(content)
+    def add_collection_item_metadata(self, collection, obj_id, content):
+        if gen_id := self._get_collection_item_gen_id_by_identifier(collection, obj_id):
+            self.collections[collection][gen_id]["metadata"].append(content)
             return content
         return None
 
-    def add_mediafile_to_entity(self, collection, id, mediafile_id):
-        collection_item = self._get_collection_item_by_id(collection, id)
-        if collection_item:
-            identifiers = collection_item["identifiers"]
-            if collection_item["_id"] not in identifiers:
-                identifiers.append(collection_item["_id"])
+    def add_mediafile_to_entity(self, collection, obj_id, mediafile_id):
+        if item := self.get_item_from_collection_by_id(collection, obj_id):
+            identifiers = item["identifiers"]
+            if item["_id"] not in identifiers:
+                identifiers.append(item["_id"])
             self.collections["mediafiles"][mediafile_id][collection] = identifiers
             return self.collections["mediafiles"][mediafile_id]
         return None
 
     def save_item_to_collection(self, collection, content):
-        id = str(uuid.uuid4())
-        content["_id"] = id
-        self.collections[collection][id] = content
-        return self.collections[collection][id]
+        gen_id = str(uuid.uuid4())
+        content["_id"] = gen_id
+        self.collections[collection][gen_id] = content
+        return self.collections[collection][gen_id]
 
-    def update_item_from_collection(self, collection, id, content):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        if main_id:
-            self.collections[collection][main_id] = content
-            return self.collections[collection][main_id]
+    def update_item_from_collection(self, collection, obj_id, content):
+        if gen_id := self._get_collection_item_gen_id_by_identifier(collection, obj_id):
+            self.collections[collection][gen_id] = content
+            return self.collections[collection][gen_id]
         return None
 
-    def update_collection_item_metadata(self, collection, id, content):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        if main_id:
-            self.collections[collection][main_id]["metadata"] = content
-            return content
+    def update_collection_item_metadata(self, collection, obj_id, content):
+        if gen_id := self._get_collection_item_gen_id_by_identifier(collection, obj_id):
+            self.collections[collection][gen_id]["metadata"] = content
+            return self.collections[collection][gen_id]["metadata"]
         return None
 
-    def patch_item_from_collection(self, collection, id, content):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        if main_id:
-            item = self.collections[collection][main_id]
+    def patch_item_from_collection(self, collection, obj_id, content):
+        if gen_id := self._get_collection_item_gen_id_by_identifier(collection, obj_id):
             for key in content:
-                item[key] = content[key]
-            return item
+                self.collections[collection][gen_id][key] = content[key]
+            return self.collections[collection][gen_id]
         return None
 
-    def delete_item_from_collection(self, collection, id):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        self.collections[collection].pop(main_id, None)
+    def delete_item_from_collection(self, collection, obj_id):
+        gen_id = self._get_collection_item_gen_id_by_identifier(collection, obj_id)
+        self.collections[collection].pop(gen_id, None)
 
-    def delete_collection_item_metadata_key(self, collection, id, key):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        if main_id:
-            self.collections[collection][main_id]["metadata"] = list(
+    def delete_collection_item_metadata_key(self, collection, obj_id, key):
+        if gen_id := self._get_collection_item_gen_id_by_identifier(collection, obj_id):
+            self.collections[collection][gen_id]["metadata"] = list(
                 filter(
                     lambda elem: elem["key"] != key,
-                    self.get_collection_item_metadata(collection, main_id),
+                    self.collections[collection][gen_id]["metadata"],
                 )
             )
 
-    def drop_collection(self, collection):
-        self.collections[collection].clear()
-
     def drop_all_collections(self):
-        for collection in self.collections:
-            self.drop_collection(collection)
+        [self.collections[collection].clear() for collection in self.collections]
 
-    def _get_collection_item_by_id(self, collection, id):
-        main_id = self._get_collection_item_main_id_by_identifier(collection, id)
-        if main_id:
-            return self.collections[collection][main_id]
-        return None
-
-    def _get_collection_item_main_id_by_identifier(self, collection, id):
-        for collection_item in self.collections[collection].values():
-            if (
-                "identifiers" in collection_item
-                and id in collection_item["identifiers"]
-            ) or collection_item["_id"] == id:
-                return collection_item["_id"]
+    def _get_collection_item_gen_id_by_identifier(self, collection, obj_id):
+        for item in self.collections[collection].values():
+            if ("identifiers" in item and obj_id in item["identifiers"]) or item[
+                "_id"
+            ] == obj_id:
+                return item["_id"]
         return None
