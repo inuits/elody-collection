48b8cacc7a3213800c947f8469cb1b7e42a3acce
Gijs Vermeulen <gverm@inuits.eu>
2021-07-14 14:39:39 +0200

    Refs #79701 refactor and add remaining importer tests

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -30,18 +30,17 @@ class ImporterTest(BaseCase):
         self.assertEqual(200, response.status_code)
         return response
 
-    def validate_db_response(self, endpoint, expected_count):
+    def run_test(self, folder, entity_count, mediafile_count):
+        self.import_csv(folder)
+        self.validate_db_response("/entities", "entity", entity_count)
+        self.validate_db_response("/mediafiles", "mediafile", mediafile_count)
+
+    def validate_db_response(self, endpoint, entity_type, count):
         response = self.get_from_db(endpoint)
-        self.assertEqual(response.json["count"], expected_count)
+        self.assertEqual(response.json["count"], count)
         self.assertEqual(len(response.json["results"]), response.json["count"])
         for obj in response.json["results"]:
-            self.validate_object(endpoint[1:], obj)
-
-    def validate_object(self, type, obj):
-        if type == "entities":
-            self.validate_entity(obj)
-        elif type == "mediafiles":
-            self.valid_mediafile(obj)
+            self.validate_object(entity_type, obj)
 
     def validate_entity(self, entity):
         self.assertEqual(str, type(entity["_id"]))
@@ -51,11 +50,18 @@ class ImporterTest(BaseCase):
 
     def validate_metadata(self, metadata):
         self.assertEqual(list, type(metadata))
+        self.assertTrue(len(metadata) > 0)
         for elem in metadata:
             if ("key", "rights") in elem.items():
                 self.assertEqual(str, type(elem["value"]))
             elif ("key", "copyright") in elem.items():
                 self.assertEqual(str, type(elem["value"]))
+
+    def validate_object(self, obj_type, obj):
+        if obj_type == "entity":
+            self.validate_entity(obj)
+        elif obj_type == "mediafile":
+            self.valid_mediafile(obj)
 
     def test_get_directories(self):
         response = self.get_from_db("/importer/directories")
@@ -69,27 +75,17 @@ class ImporterTest(BaseCase):
         )
 
     def test_import_bad_files(self):
-        self.import_csv("empty")
-        self.validate_db_response("/entities", 0)
-        self.validate_db_response("/mediafiles", 0)
-        self.import_csv("malformed_columns")
-        self.validate_db_response("/entities", 0)
-        self.validate_db_response("/mediafiles", 0)
-        self.import_csv("malformed_rows")
-        self.validate_db_response("/entities", 0)
-        self.validate_db_response("/mediafiles", 0)
+        self.run_test("empty", 0, 0)
+        self.run_test("malformed_columns", 0, 0)
+        self.run_test("malformed_rows", 0, 0)
+
+    # mediafile_count is wrong in the following tests as we don't check for duplicate files yet
 
     def test_import_csv_columns_casing(self):
-        self.import_csv("column_casing")
-        self.validate_db_response("/entities", 2)
-        self.validate_db_response("/mediafiles", 2)
+        self.run_test("column_casing", 2, 2)
 
     def test_import_csv_path_types(self):
-        self.import_csv("path_types")
-        self.validate_db_response("/entities", 2)
-        self.validate_db_response("/mediafiles", 2)
+        self.run_test("path_types", 2, 2)
 
     def test_import_csv_metadata(self):
-        self.import_csv("metadata")
-        self.validate_db_response("/entities", 4)
-        self.validate_db_response("/mediafiles", 6)
+        self.run_test("metadata", 4, 7)
