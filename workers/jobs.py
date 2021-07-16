d0fa9a9b6c1ef2447274529e5bfb477271421ffc
SK <sri@inuits.eu>
2021-07-16 20:24:18 +0200

    add file signature genarator and checker

--- workers/jobs.py
+++ workers/jobs.py
@@ -1,80 +1,107 @@
-import app
+import hashlib
 import json
 import os
-import requests
+import uuid
 
-from storage.storagemanager import MongoStorageManager as storage_manager
+import requests
+from flask_restful import abort
+
+import app
+from storage.mongostore import MongoStorageManager
 
 storage_api = os.getenv("STORAGE_API_URL", "http://localhost:8001")
 mount_point = os.getenv("MOUNT_POINT", "")
+location = os.getenv("UPLOAD_FOLDER", "/mnt/media-import")
+
+
+class DBFetch(MongoStorageManager):
+    def is_duplicate(self, signature):
+        fetch_data = self.db["jobs"].find_one({"signature": signature})
+        return fetch_data is None
 
 
 class CreateJobs:
-    def __init__(self, job_type, job):
+    def __init__(self, job_type, job, user):
         self.job_type = job_type
         self.job = job
-        self.storage = storage_manager
-        self.db = self.storage().db
-        self.location = os.getenv("UPLOAD_FOLDER", "/mnt/media-import")
+        self.user = user
 
-    def create_job(self):
+    def create_single_job(self):
         """ creates  jobs """
-        message = {}
-        job_data = {}
-        data = list()
-        if self.job_type == "multiple":
-            # For multiple file upload prepare the data and append to a list
-            for item in self.job:
-                upload_folder = {
-                    "upload_folder": os.path.join(self.location, item["file_name"])
-                }
-                data.append(upload_folder)
+        data_fetch = DBFetch()
 
-            message["asset"] = data
-        else:
-            # For single upload
-            message["asset"] = {
-                "upload_folder": os.path.join(self.location, self.job["file_name"])
+        job_data = {
+            "job_info": self.job.get("info"),
+            "job_type": self.job_type,
+            "user": self.user,
+            "status": "Queued",
+        }
+        message_id = (str(uuid.uuid4()),)
+        if self.job_type == "single":
+            # generate file signatire
+            signature = generate_file_signature(self.job.get("asset"))
+            m_message = {
+                "data": {
+                    "job_folder": os.path.join(location, self.job.get("asset").filename)
+                },
+                "job_id": self.job_type,
+                "asset": self.job.get("asset").filename,
             }
-        message["job_type"] = self.job_type
+            job_data["signature"] = signature
 
-        save_job = self.storage().save_item_to_collection(
-            "jobs", self.job
-        )  # Save the job to collection
-        message["job_id"] = save_job["_id"]
-        app.ramq.send(
-            message,
-            exchange_name="dams",
-            routing_key="dams.import_start",
-        )
+            if data_fetch.is_duplicate(signature):
+                save_job = data_fetch.save_item_to_collection("jobs", job_data)
+                m_message["job_id"] = save_job["_id"]
+            else:
+                abort(409, message=f'File {self.job.get("asset").filename} exists')
 
-        return message, 201
+            m_message["message_id"] = message_id
+            app.ramq.send(
+                m_message,
+                exchange_name=os.getenv("EXCHANGE_NAME"),
+                routing_key=os.getenv("ROUTING_KEY"),
+            )
+            return m_message, 201
+        else:
+            return {'message': "This end-point is in development"}
 
 
 @app.ramq.queue(
-    exchange_name="dams",
-    routing_key="dams.import_start",
+    exchange_name=os.getenv("EXCHANGE_NAME"),
+    routing_key=os.getenv("ROUTING_KEY"),
 )
 def job_status(body):
     data = json.loads(body)
-
+    data_fetcher = DBFetch()
     # fetch job from collection
-    job = storage_manager().get_jobs_from_collection("jobs", data["job_id"])
-
+    job = data_fetcher.get_jobs_from_collection("jobs", data["job_id"])
+    job["status"] = "In-Progress"
+    # Update Job Status
+    data_fetcher.patch_item_from_collection("jobs", job["job_id"], job)
     # process multiple jobs
     if data["job_type"] == "multiple":
         for data in data:
-            save_file = process_data(mount_point + data["asset"])
-            job["status"] = "finished" if save_file.status_code == 201 else "failed"
-
+            save = process_data(mount_point + data["asset"])
+            job["status"] = "Finished" if save.status_code is 201 else "Failed"
     else:
         # process single jobs
         save_file = process_data(mount_point + data["asset"])
-        job["status"] = "finished" if save_file.status_code == 201 else "failed"
-    storage_manager().patch_item_from_collection("jobs", job["job_id"], job)
+        job["status"] = "Finished" if save_file.status_code is 201 else "Failed"
+    # Update Job Status
+    data_fetcher.patch_item_from_collection("jobs", job["job_id"], job)
     return True
 
 
 def process_data(path):
     file_name = os.path.basename(path)
     return requests.post(f"{storage_api}/upload/{os.path.basename(file_name)}")
+
+
+def generate_file_signature(file):
+    obj = hashlib.md5()
+    size = 128 * obj.block_size
+    parts = file.read(size)
+    while parts:
+        obj.update(parts)
+        parts = file.read(size)
+    return obj.hexdigest()
