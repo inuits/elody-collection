44d8e9ec22de4bf460a160ab373d8d5db7ec4789
SK <sri@inuits.eu>
2021-07-19 13:39:36 +0200

    refactor and update job_status.py

--- resources/job_status.py
+++ resources/job_status.py
@@ -1,25 +1,11 @@
-import json
-import os
-
-import werkzeug.datastructures
-
 import app
 
-from flask import request, g
+from flask import g
 from flask_restful import reqparse
 from resources.base_resource import BaseResource
-from storage.storagemanager import MongoStorageManager as storage
-from werkzeug.exceptions import abort
-from workers.jobs import CreateJobs
+from resources.jobs import generate_file_signature
 
-
-def abort_if_not_exist(option, target):
-    """Checks if requested item exists - items can either be job ID, user, or asset"""
-    job = storage().get_jobs_from_collection("jobs", target)
-    if job:
-        return job  # returns a pymongo cursor
-    else:
-        abort(404, f"job for {option} : {target} Not Found in jobs Collection")
+import werkzeug.datastructures
 
 
 class JobStatusById(BaseResource):
@@ -27,9 +13,7 @@ class JobStatusById(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def get(self, job_id):
-        job = abort_if_not_exist("job_id", job_id)
-        for item in job:
-            return item
+        return self.abort_if_item_doesnt_exist("jobs", job_id)
 
 
 class JobsByAsset(BaseResource):
@@ -37,11 +21,7 @@ class JobsByAsset(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def get(self, asset):
-        jobs = list()
-        data = abort_if_not_exist(option="user", target=asset)
-        for job in data:
-            jobs.append(job)
-        return jobs
+        return self.abort_if_item_doesnt_exist("jobs", asset)
 
 
 class JobStatusByUser(BaseResource):
@@ -49,29 +29,12 @@ class JobStatusByUser(BaseResource):
         require_token=BaseResource.token_required, scopes_required=["openid"]
     )
     def get(self):
-        user = g.oidc_token_info["email"]
-        jobs = list()
-        data = abort_if_not_exist(option="user", target=user)
-        for job in data:
-            jobs.append(job)
-        return jobs
-
-    @app.oidc.accept_token(
-        require_token=BaseResource.token_required, scopes_required=["openid"]
-    )
-    def post(self):
-        post_data = request.get_json()
-        if post_data:
-            return CreateJobs(
-                job_type="multiple" if isinstance(post_data, list) else "single",
-                job=post_data,
-            ).create_single_job()
-        else:
-            abort(405, "Request failed")
+        user = "user"
+        return self.abort_if_item_doesnt_exist("jobs", user)
 
 
 # Upload single file
-class JobUploadSingleItem(BaseResource, CreateJobs):
+class JobUploadSingleItem(BaseResource):
     """Upload single file endpoint"""
 
     @app.oidc.accept_token(
@@ -87,13 +50,13 @@ class JobUploadSingleItem(BaseResource, CreateJobs):
             required=True,
         )
         parse.add_argument("info", required=True, help="File Information")
-        self.job = parse.parse_args()
-        self.job_type = "single"
-        return self.create_single_job()
+        self.req = parse.parse_args()
+        signature = generate_file_signature(self.req.get("asset"))
+        return self.create_single_job(signature)
 
 
 # Upload multiple files
-class JobUploadMultipleItem(BaseResource, CreateJobs):
+class JobUploadMultipleItem(BaseResource):
     """ Upload Multiple files"""
 
     @app.oidc.accept_token(
@@ -110,7 +73,5 @@ class JobUploadMultipleItem(BaseResource, CreateJobs):
             action="append",  # grabs multiple files
         )
         parse.add_argument("info", required=True)
-        self.job = parse.parse_args()
-        self.job_type = "multiple"
-        self.user = g.oidc_token_info["email"]
+        self.req = parse.parse_args()
         return self.create_multiple_jobs()
