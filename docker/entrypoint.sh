6af9c1e80e4ee706057887ed9ddeb8865df1287b
Tom Van den Abbeele <tom.vda@inuits.eu>
2021-08-25 00:10:20 +0200

    Improving container image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+if [ ! -z "$@" ]; then
+  echo "Running command: $@"
+  exec $@
+  exit $?
+fi
+
 if [ "$APP_ENV" = "dev" ]; then
   echo "Starting development server..."
-  exec flask run --host=0.0.0.0
+  exec flask run --host=0.0.0.0 --port=8000
 else
   echo "Starting gunicorn server..."
   exec gunicorn -b 0.0.0.0:8000 app:app
