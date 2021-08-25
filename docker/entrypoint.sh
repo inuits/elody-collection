ce87b5787d089e52da46a081436758b36bd0f031
Matthias Dillen <matthias.dillen@inuits.eu>
2021-08-25 14:36:10 +0200

    Revert "Improving container image"
    This reverts commit 6af9c1e80e4ee706057887ed9ddeb8865df1287b.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,15 +2,9 @@
 
 set -e
 
-if [ ! -z "$@" ]; then
-  echo "Running command: $@"
-  exec $@
-  exit $?
-fi
-
 if [ "$APP_ENV" = "dev" ]; then
   echo "Starting development server..."
-  exec flask run --host=0.0.0.0 --port=8000
+  exec flask run --host=0.0.0.0
 else
   echo "Starting gunicorn server..."
   exec gunicorn -b 0.0.0.0:8000 app:app
