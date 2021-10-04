f7a34fbac5bf8ea12593dbab12f96f713537fd17
Bram Oosterlynck <booster@inuits.eu>
2021-10-04 15:24:40 +0200

    Add cantaloupe_api_url to test script

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -3,6 +3,7 @@
 export DB_ENGINE=$1
 export REQUIRE_TOKEN=0
 export STORAGE_API_URL=https://dams-storage-api.inuits.io
+export CANTALOUPE_API_URL=https://dams-image-api.inuits.io
 
 cat << EOF
 ============================================
