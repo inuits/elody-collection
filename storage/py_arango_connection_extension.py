9d01658c01bea753d8fcb241242dc46ddd66b6de
Gijs Vermeulen <gverm@inuits.eu>
2021-10-07 16:25:43 +0200

    Small arango connection extention class cleanup

--- storage/py_arango_connection_extension.py
+++ storage/py_arango_connection_extension.py
@@ -6,12 +6,12 @@ from pyArango.theExceptions import CreationError
 
 
 class PyArangoConnection(Connection, ABC):
-    def create_helper(self, name, db_name, endpoint, args, expected_status):
+    def create_helper(self, name, db_name, db_collection, args, expected_status):
         if args is None:
             args = {}
         args["name"] = name
         payload = json.dumps(args, default=str)
-        url = "{}/_db/{}/{}".format(self.arangoURL[0], db_name, endpoint)
+        url = "{}/_db/{}/_api/{}".format(self.getEndpointURL(), db_name, db_collection)
         r = self.session.post(url, data=payload)
         data = r.json()
         if r.status_code == expected_status and not data["error"]:
@@ -20,13 +20,13 @@ class PyArangoConnection(Connection, ABC):
             raise CreationError(data["errorMessage"], r.content)
 
     def createCollection(self, name, db_name, args=None):
-        return self.create_helper(name, db_name, "/_api/collection", args, 200)
+        return self.create_helper(name, db_name, "collection", args, 200)
 
     def createEdge(self, name, db_name, args=None):
         if args is None:
             args = {}
         args["type"] = 3
-        return self.create_helper(name, db_name, "/_api/collection", args, 200)
+        return self.create_helper(name, db_name, "collection", args, 200)
 
     def createGraph(self, name, db_name, args=None):
-        return self.create_helper(name, db_name, "/_api/gharial", args, 202)
+        return self.create_helper(name, db_name, "gharial", args, 202)
