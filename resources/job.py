72cdeb1f64f325f4b0d2a855d557ec038190716d
Matthias Dillen <matthias.dillen@inuits.eu>
2021-10-12 09:43:00 +0200

    fix child job selection

--- resources/job.py
+++ resources/job.py
@@ -64,7 +64,7 @@ class JobDetail(BaseResource):
     def get(self, id):
         job = self.abort_if_item_doesnt_exist("jobs", id)
         if job["parent_job_id"] == "":
-            sub_jobs = self.storage.get_items_from_collection_by_fields("jobs", {"parent_job_id": job["_id"]})
+            sub_jobs = self.storage.get_items_from_collection_by_fields("jobs", {"parent_job_id": job["_key"]})
             job["sub_jobs"] = sub_jobs["results"]
         return job
 
