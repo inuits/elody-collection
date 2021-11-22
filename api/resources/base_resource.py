93b836c88a2b7668d634875d61ac3a0735dc1778
Gijs Vermeulen <gverm@inuits.eu>
2021-11-22 12:03:56 +0100

    Remove unused token_required variable

--- api/resources/base_resource.py
+++ api/resources/base_resource.py
@@ -12,8 +12,6 @@ from werkzeug.exceptions import BadRequest
 
 
 class BaseResource(Resource):
-    token_required = os.getenv("REQUIRE_TOKEN", "True").lower() in ["true", "1"]
-
     def __init__(self):
         self.storage = StorageManager().get_db_engine()
         self.collection_api_url = os.getenv(
