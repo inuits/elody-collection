a49821b556a22f3c6bb2b7d152361cde5e8e5827
Matthias Dillen <matthias.dillen@inuits.eu>
2021-12-09 09:31:03 +0100

    Refs #86462

--- api/storage/arangostore.py
+++ api/storage/arangostore.py
@@ -55,7 +55,7 @@ FOR c IN entities
     LET primary_items = (
         FOR item, edge IN OUTBOUND c hasMediafile
             FILTER edge.is_primary == true || edge.is_primary_thumbnail == true
-            LET primary = edge.is_primary != true ? null : {primary_mediafile_location: item.original_file_location}
+            LET primary = edge.is_primary != true ? null : {primary_mediafile_location: item.original_file_location, primary_mediafile: item.filename}
             LET primary_thumb = edge.is_primary_thumbnail != true ? null : {primary_thumbnail_location: item.thumbnail_file_location}
             RETURN primary != null AND primary_thumb != null ? MERGE(primary, primary_thumb) : (primary ? primary : primary_thumb)
     )
