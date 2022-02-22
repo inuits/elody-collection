2963d9ffc366044a221b397751c8704f7834466f
Bram Oosterlynck <booster@inuits.eu>
2022-02-22 10:36:58 +0100

    Use singelton pattern to only initialize storage engine on startup

--- api/storage/storagemanager.py
+++ api/storage/storagemanager.py
@@ -3,16 +3,20 @@ import os
 from storage.arangostore import ArangoStorageManager
 from storage.memorystore import MemoryStorageManager
 from storage.mongostore import MongoStorageManager
+from singleton import Singleton
 
-
-class StorageManager:
+class StorageManager(metaclass=Singleton):
     def __init__(self):
         self.storage_engine = os.getenv("DB_ENGINE", "arango")
+        self.__init_storage_managers()
 
     def get_db_engine(self):
+        return self.storage_manager
+
+    def __init_storage_managers(self):
         if self.storage_engine == "arango":
-            return ArangoStorageManager()
+            self.storage_manager = ArangoStorageManager()
         elif self.storage_engine == "memory":
-            return MemoryStorageManager()
+            self.storage_manager = MemoryStorageManager()
         elif self.storage_engine == "mongo":
-            return MongoStorageManager()
+            self.storage_manager = MongoStorageManager()
