96ddc2c3d315e6044eac63a95a1eb6d8b7013eaf
Gijs Vermeulen <gverm@inuits.eu>
2022-05-12 14:49:08 +0200

    Fix format string blocks

--- api/storage/arangostore.py
+++ api/storage/arangostore.py
@@ -51,22 +51,26 @@ class ArangoStorageManager:
     def get_box_visits(self, skip, limit, item_type=None, ids=None):
         aql = f"""
             FOR c IN box_visits
-                {"FILTER c._key IN @ids" if ids else ""}
-                {f'FILTER c.type == "{item_type}"' if item_type else ""}
-                LET new_metadata = (
-                    FOR item,edge IN OUTBOUND c GRAPH 'assets'
-                        FILTER edge._id NOT LIKE 'hasMediafile%'
-                        LET relation = {'key': edge._to, 'type': edge.type}
-                        RETURN HAS(edge, 'label') ? MERGE(relation, {'label': IS_NULL(edge.label.`@value`) ? edge.label : edge.label.`@value`}) : relation
-                )
-                LET all_metadata = {'metadata': APPEND(c.metadata, new_metadata)}
-                LIMIT @skip, @limit
-                RETURN MERGE(c, all_metadata)
+            {"FILTER c._key IN @ids" if ids else ""}
+            {f'FILTER c.type == "{item_type}"' if item_type else ""}
+        """
+        aql2 = """
+            LET new_metadata = (
+                FOR item,edge IN OUTBOUND c GRAPH 'assets'
+                    FILTER edge._id NOT LIKE 'hasMediafile%'
+                    LET relation = {'key': edge._to, 'type': edge.type}
+                    RETURN HAS(edge, 'label') ? MERGE(relation, {'label': IS_NULL(edge.label.`@value`) ? edge.label : edge.label.`@value`}) : relation
+            )
+            LET all_metadata = {'metadata': APPEND(c.metadata, new_metadata)}
+            LIMIT @skip, @limit
+            RETURN MERGE(c, all_metadata)
         """
         bind = {"skip": skip, "limit": limit}
         if ids:
             bind["ids"] = ids
-        results = self.db.AQLQuery(aql, rawResults=True, bindVars=bind, fullCount=True)
+        results = self.db.AQLQuery(
+            f"{aql}{aql2}", rawResults=True, bindVars=bind, fullCount=True
+        )
         items = dict()
         items["count"] = results.extra["stats"]["fullCount"]
         items["results"] = list(results)
