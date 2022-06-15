135be44838bf33069b9925a677d7e66499407503
Gijs Vermeulen <gverm@inuits.eu>
2022-06-15 14:37:57 +0200

    Fix setting/getting relations of new frame (storybox)

--- api/resources/story_box.py
+++ api/resources/story_box.py
@@ -17,7 +17,7 @@ class StoryBoxLink(BaseResource):
         box_visit = self.abort_if_item_doesnt_exist("box_visits", code)
         content = {
             "type": "frame",
-            "metadata": {"key": "type", "value": "frame", "language": "en"},
+            "metadata": [{"key": "type", "value": "frame", "language": "en"}],
             "user": current_token["email"],
         }
         frame = self.storage.save_item_to_collection("entities", content)
@@ -28,8 +28,5 @@ class StoryBoxLink(BaseResource):
         new_relations = [
             {"key": item, "label": "asset", "type": "components"} for item in in_basket
         ]
-        new_relations = self.storage.add_relations_to_collection_item(
-            "entities", self._get_raw_id(frame), new_relations
-        )
-        frame["metadata"] |= new_relations
-        return frame, 201
+        self.storage.add_relations_to_collection_item("entities", self._get_raw_id(frame), new_relations)
+        return self._add_relations_to_metadata(frame), 201
