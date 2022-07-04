e686f717e4def12f0282a57095891b456549b0d6
Gijs Vermeulen <gverm@inuits.eu>
2022-07-04 17:57:39 +0200

    Refs #95818 factor out box_visit creation

--- api/resources/box_visit.py
+++ api/resources/box_visit.py
@@ -1,8 +1,6 @@
 import app
 
-from datetime import datetime
 from flask import request, after_this_request
-from flask_restful import abort
 from resources.base_resource import BaseResource
 from validator import box_visit_schema
 
@@ -11,45 +9,7 @@ class BoxVisit(BaseResource):
     @app.require_oauth("create-box-visit")
     def post(self):
         content = self.get_request_body()
-        if "story_id" not in content:
-            abort(405, message="Invalid input")
-        story = self.abort_if_item_doesnt_exist("entities", content["story_id"])
-        story = self._add_relations_to_metadata(story)
-        num_frames = sum(
-            map(lambda x: "type" in x and x["type"] == "frames", story["metadata"])
-        )
-        code = self.storage.generate_box_visit_code()
-        box_visit = {
-            "type": "box_visit",
-            "identifiers": [code],
-            "code": code,
-            "start_time": datetime.now().isoformat(),
-            "metadata": [],
-            "frames_seen_last_visit": 0,
-            "touch_table_time": None,
-        }
-        box_visit = self.storage.save_item_to_collection("box_visits", box_visit)
-        relation = {
-            "type": "stories",
-            "label": "story",
-            "key": story["_id"],
-            "active": True,
-            "total_frames": num_frames,
-            "order": 0,
-            "last_frame": "",
-        }
-        self.storage.add_relations_to_collection_item(
-            "box_visits", self._get_raw_id(box_visit), [relation], False
-        )
-        relation = {
-            "type": "story_box_visits",
-            "label": "box_visit",
-            "key": box_visit["_id"],
-        }
-        self.storage.add_relations_to_collection_item(
-            "entities", self._get_raw_id(story), [relation], False
-        )
-        return self._add_relations_to_metadata(box_visit, "box_visits", sort_by="order")
+        return self._create_box_visit(content)
 
     @app.require_oauth("read-box-visit")
     def get(self):
