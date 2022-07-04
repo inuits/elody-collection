94fab28218082adca23525b75c2879ab9322ee78
Gijs Vermeulen <gverm@inuits.eu>
2022-07-04 16:42:49 +0200

    Refs #95806 rework linking box code to story box

--- api/resources/story_box.py
+++ api/resources/story_box.py
@@ -15,20 +15,23 @@ class StoryBoxLink(BaseResource):
     @app.require_oauth("link-story-box")
     def post(self, code):
         box_visit = self.abort_if_item_doesnt_exist("box_visits", code)
-        content = {
-            "type": "frame",
-            "metadata": [{"key": "type", "value": "frame", "language": "en"}],
-            "user": current_token["email"],
-        }
-        frame = self.storage.save_item_to_collection("entities", content)
         relations = self.storage.get_collection_item_relations(
             "box_visits", self._get_raw_id(box_visit)
         )
-        in_basket = [x["key"] for x in relations if x["type"] == "inBasket"]
-        new_relations = [
-            {"key": item, "label": "asset", "type": "components"} for item in in_basket
-        ]
-        self.storage.add_relations_to_collection_item(
-            "entities", self._get_raw_id(frame), new_relations
-        )
-        return self._add_relations_to_metadata(frame), 201
+        if story_box := next((x for x in relations if x["type"] == "story_box"), None):
+            content = {"user": current_token["email"]}
+            story_box = self.storage.patch_item_from_collection(
+                "entities", self._get_raw_id(story_box), content
+            )
+        else:
+            content = {
+                "type": "frame",
+                "metadata": [{"key": "type", "value": "frame", "language": "en"}],
+                "user": current_token["email"],
+            }
+            story_box = self.storage.save_item_to_collection("entities", content)
+            content = [{"key": story_box["_id"], "type": "story_box"}]
+            self.storage.patch_collection_item_relations(
+                "box_visits", self._get_raw_id(box_visit), content, False
+            )
+        return story_box, 201
