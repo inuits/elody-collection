e29d9d7f2c2517d6f7e00baf2b007d6cd38b9d3b
Bram Oosterlynck <booster@inuits.eu>
2022-08-09 11:40:35 +0200

    Refs #98030 format app-loader

--- api/apps/loader.py
+++ api/apps/loader.py
@@ -4,6 +4,7 @@ import os
 from importlib import import_module
 from yaml import safe_load, YAMLError
 
+
 def load_apps(flask_app):
     apps = parse_apps()
     for app in apps:
@@ -11,15 +12,16 @@ def load_apps(flask_app):
             api_bp = import_module(f"apps.{app}.resources.{resource}").api_bp
             flask_app.register_blueprint(api_bp)
 
+
 def parse_apps():
     apps = dict()
     apps_manifest = os.getenv("APPS_MANIFEST")
     if not os.path.exists(apps_manifest):
         app.logger.error(f"Applist not found")
         return apps
-    with open(apps_manifest, "r") as stream:
+    with open(apps_manifest, "r") as file:
         try:
-            apps = safe_load(stream)
+            apps = safe_load(file)
         except YAMLError as ex:
             app.logger.error(f"Could not load applist: {ex}")
     return apps
