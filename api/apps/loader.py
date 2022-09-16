ccc74d1a9504f050c29e962e12d80b5bef190154
Gijs Vermeulen <gverm@inuits.eu>
2022-09-16 13:44:34 +0200

    Use JSON instead of YAML for app definitions
    JSON doesn't require extra dependencies, as it's a built-in module.

--- api/apps/loader.py
+++ api/apps/loader.py
@@ -1,8 +1,8 @@
 import app
+import json
 import os
 
 from importlib import import_module
-from yaml import safe_load, YAMLError
 
 
 def load_apps(flask_app):
@@ -15,13 +15,13 @@ def load_apps(flask_app):
 
 def parse_apps():
     apps = dict()
-    apps_manifest = os.getenv("APPS_MANIFEST", "apps/app_list.yaml")
+    apps_manifest = os.getenv("APPS_MANIFEST", "apps/app_list.json")
     if not os.path.exists(apps_manifest):
         app.logger.error(f"Applist not found")
         return apps
     with open(apps_manifest, "r") as file:
         try:
-            apps = safe_load(file)
-        except YAMLError as ex:
+            apps = json.load(file)
+        except json.JSONDecodeError as ex:
             app.logger.error(f"Could not load applist: {ex}")
     return apps
