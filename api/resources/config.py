3c9bf842ce30d9023754e32210fd0f61baf55dc3
Bram Oosterlynck <booster@inuits.eu>
2022-10-26 16:01:17 +0200

    Refs #102408 use hardcoded values when needed

--- api/resources/config.py
+++ api/resources/config.py
@@ -7,15 +7,28 @@ class Config(BaseResource):
     @app.require_oauth("read-config")
     def get(self):
         config = dict()
+        hard_coded_values = {
+            "publication_status": [
+                "publiek",
+                "niet_publiek",
+                "kandidaat",
+                "werkbeeld",
+                "beschermd",
+                "expliciet",
+            ],
+        }
         keys_for_config = {
             "mediafiles": ["publication_status", "rights", "source"],
             "entities": ["publication_status"],
         }
         for collection, keys in keys_for_config.items():
             for key in keys:
-                config[
-                    f"{collection}_{key}_values"
-                ] = self.storage.get_metadata_values_for_collection_item_by_key(
-                    collection, key
-                )
+                if key in hard_coded_values:
+                    config[f"{collection}_{key}_values"] = hard_coded_values[key]
+                else:
+                    config[
+                        f"{collection}_{key}_values"
+                    ] = self.storage.get_metadata_values_for_collection_item_by_key(
+                        collection, key
+                    )
         return config
