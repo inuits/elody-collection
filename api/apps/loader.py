c9f4cf89b3f1daa927d0d72b8f71bf04a371a3e5
Gijs Vermeulen <gverm@inuits.eu>
2022-10-31 15:56:50 +0100

    Use util.read_json_as_dict in load_apps

--- api/apps/loader.py
+++ api/apps/loader.py
@@ -1,27 +1,12 @@
-import app
-import json
 import os
+import util
 
 from importlib import import_module
 
 
 def load_apps(flask_app):
-    apps = parse_apps()
+    apps = util.read_json_as_dict(os.getenv("APPS_MANIFEST", "apps/app_list.json"))
     for app in apps:
         for resource in apps[app].get("resources", []):
             api_bp = import_module(f"apps.{app}.resources.{resource}").api_bp
             flask_app.register_blueprint(api_bp)
-
-
-def parse_apps():
-    apps = dict()
-    apps_manifest = os.getenv("APPS_MANIFEST", "apps/app_list.json")
-    if not os.path.exists(apps_manifest):
-        app.logger.error(f"Applist not found")
-        return apps
-    with open(apps_manifest, "r") as file:
-        try:
-            apps = json.load(file)
-        except json.JSONDecodeError as ex:
-            app.logger.error(f"Could not load applist: {ex}")
-    return apps
