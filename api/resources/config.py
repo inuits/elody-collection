b35f3a0d6e2c875ab30bae479bb719eba43143ab
Gijs Vermeulen <gverm@inuits.eu>
2022-11-17 13:30:59 +0100

    Small config code clean up

--- api/resources/config.py
+++ api/resources/config.py
@@ -4,41 +4,56 @@ from resources.base_resource import BaseResource
 
 
 class Config(BaseResource):
+    filter_options_map = {
+        "publication_status_media_file": "mediafiles_publication_status_values",
+        "publication_status": "entities_publication_status_values",
+        "rights": "mediafiles_rights_values",
+        "source": "mediafiles_source_values",
+    }
+    hard_coded_values = {
+        "publication_status": [
+            "publiek",
+            "niet-publiek",
+            "kandidaat",
+            "werkbeeld",
+            "beschermd",
+            "expliciet",
+        ],
+    }
+    keys_for_config = {
+        "mediafiles": ["publication_status", "rights", "source"],
+        "entities": ["publication_status"],
+    }
+
+    def __add_options_to_filters(self, options, filters, filter_options_map):
+        for collection, collection_filters in filters.items():
+            for filter in collection_filters:
+                key = filter["key"]
+                if key in filter_options_map and filter_options_map[key] in options:
+                    self.__add_options_to_specific_filter(
+                        filter, options[filter_options_map[key]]
+                    )
+
+    def __add_options_to_specific_filter(self, filter, options):
+        filter["options"] = list()
+        for option in options:
+            filter["options"].append({"label": option, "value": option})
+
     @app.require_oauth("read-config")
     def get(self):
         config = dict()
-        hard_coded_values = {
-            "publication_status": [
-                "publiek",
-                "niet-publiek",
-                "kandidaat",
-                "werkbeeld",
-                "beschermd",
-                "expliciet",
-            ],
-        }
-        keys_for_config = {
-            "mediafiles": ["publication_status", "rights", "source"],
-            "entities": ["publication_status"],
-        }
-        filter_options_map = {
-            "publication_status_media_file": "mediafiles_publication_status_values",
-            "publication_status": "entities_publication_status_values",
-            "rights": "mediafiles_rights_values",
-            "source": "mediafiles_source_values",
-        }
-        for collection, keys in keys_for_config.items():
+        for collection, keys in self.keys_for_config.items():
             for key in keys:
-                if key in hard_coded_values:
-                    config[f"{collection}_{key}_values"] = hard_coded_values[key]
+                if key in self.hard_coded_values:
+                    config[f"{collection}_{key}_values"] = self.hard_coded_values[key]
                 else:
                     config[
                         f"{collection}_{key}_values"
                     ] = self.storage.get_metadata_values_for_collection_item_by_key(
                         collection, key
                     )
-        allowed_filters = self._get_allowed_filters()
-        config["filters"] = self._add_options_to_filters(
-            config, allowed_filters, filter_options_map
+        config["filters"] = self._get_allowed_filters()
+        self.__add_options_to_filters(
+            config, config["filters"], self.filter_options_map
         )
         return config
