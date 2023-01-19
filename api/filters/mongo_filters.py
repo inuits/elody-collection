5cdb28252739257a3d8b5bece398f5bd5076fa16
Bram Oosterlynck <booster@inuits.eu>
2023-01-19 15:29:27 +0100

    Refs #104924 add min max relations filter

--- api/filters/mongo_filters.py
+++ api/filters/mongo_filters.py
@@ -42,12 +42,16 @@ class MongoFilters(MongoStorageManager):
             elif query.get("type") == "MinMaxInput":
                 if "metadata_field" in query:
                     pipeline += self.__generate_min_max_metadata_filter(query)
-        pipeline.append({
-            "$project": {
-                "relationDocuments": 0,
-                "numberOfRelations": 0,
+                elif "relation_types" in query:
+                    pipeline += self.__generate_min_max_relations_filter(query)
+        pipeline.append(
+            {
+                "$project": {
+                    "relationDocuments": 0,
+                    "numberOfRelations": 0,
+                }
             }
-        })
+        )
         return pipeline
 
     def __generate_min_max_metadata_filter(self, query):
@@ -64,6 +68,30 @@ class MongoFilters(MongoStorageManager):
             "$gte": min,
             "$lte": max,
         }
+        return sub_pipeline
+
+    def __generate_min_max_relations_filter(self, query):
+        relation_types = self.__map_relation_types(query["relation_types"])
+        min = query.get("value", {}).get("min", -1)
+        max = query.get("value", {}).get("max", sys.maxsize)
+        relation_match = {"$match": {"relations.type": {"$in": relation_types}}}
+        number_of_relations_calculator = {
+            "$addFields": {
+                "numberOfRelations": {
+                    "$size": {
+                        "$filter": {
+                            "input": "$relations",
+                            "as": "el",
+                            "cond": {"$in": ["$$el.type", relation_types]},
+                        }
+                    }
+                }
+            }
+        }
+        min_max_match = {"$match": {"numberOfRelations": {"$gte": min, "$lte": max}}}
+        sub_pipeline = [relation_match, number_of_relations_calculator, min_max_match]
+        if len(query.get("item_types", [])):
+            sub_pipeline.append(self.__get_item_types_query(query["item_types"]))
         return sub_pipeline
 
     def __generate_multi_select_input_query(self, query):
@@ -92,9 +120,6 @@ class MongoFilters(MongoStorageManager):
                 }
             }
         }
-
-    def __get_min_max_filter_query(self, relation_types, prev_collection, min, max):
-        pass
 
     def __get_multi_select_metadata_filter(self, query):
         match_field = (
@@ -148,7 +173,14 @@ class MongoFilters(MongoStorageManager):
         }
 
     def __map_relation_types(self, relation_types):
-        pass
+        relation_types_map = {
+            "mediafiles": "hasMediafile",
+            "testimonies": "hasTestimony",
+        }
+        return [
+            relation_types_map.get(relation_type, relation_type)
+            for relation_type in relation_types
+        ]
 
     def __text_input_filter_exception(self, query, counter, prev_collection):
         pass
