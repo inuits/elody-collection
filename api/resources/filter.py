240ba3d2bc587d69a528f3b9016c5e56cde3427a
Bram Oosterlynck <booster@inuits.eu>
2023-03-07 17:25:04 +0100

    Refs #105000 allow exporting advanced filter output as CSV

--- api/resources/filter.py
+++ api/resources/filter.py
@@ -1,4 +1,5 @@
 import app
+import mappers
 
 from flask import request
 from resources.base_filter_resource import BaseFilterResource
@@ -7,14 +8,42 @@ from resources.base_filter_resource import BaseFilterResource
 class FilterEntities(BaseFilterResource):
     @app.require_oauth("search-advanced")
     def post(self):
+        accept_header = request.headers.get("Accept")
         query = request.get_json()
-        return self._execute_advanced_search_with_query(query, "entities")
+        fields = [
+            *request.args.getlist("field"),
+            *request.args.getlist("field[]"),
+        ]
+        entities = self._execute_advanced_search_with_query(query, "entities")
+        return self._create_response_according_accept_header(
+            mappers.map_data_according_to_accept_header(
+                entities,
+                accept_header,
+                "entities",
+                fields,
+            ),
+            accept_header,
+        )
 
 
 class FilterEntitiesBySavedSearchId(BaseFilterResource):
     @app.require_oauth("search-advanced")
     def post(self, id):
-        return self._execute_advanced_search_with_saved_search(id, "entities")
+        accept_header = request.headers.get("Accept")
+        fields = [
+            *request.args.getlist("field"),
+            *request.args.getlist("field[]"),
+        ]
+        entities = self._execute_advanced_search_with_saved_search(id, "entities")
+        return self._create_response_according_accept_header(
+            mappers.map_data_according_to_accept_header(
+                entities,
+                accept_header,
+                "entities",
+                fields,
+            ),
+            accept_header,
+        )
 
 
 class FilterMediafiles(BaseFilterResource):
