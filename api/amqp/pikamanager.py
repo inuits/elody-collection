09bc074a6c2d6a454c328e82b5e1262339f42dc4
Gijs Vermeulen <gverm@inuits.eu>
2023-03-29 19:52:13 +0200

    TEST: make queues not durable

--- api/amqp/pikamanager.py
+++ api/amqp/pikamanager.py
@@ -1,6 +1,7 @@
 from rabbitmq_pika_flask import RabbitMQ
+from rabbitmq_pika_flask.QueueParams import QueueParams
 
 
 class PikaAmqpManager(RabbitMQ):
     def __init__(self):
-        super().__init__()
+        super().__init__(queue_params=QueueParams(False, False, False))
