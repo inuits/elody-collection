4638c69c6dd300e11800a36601131811567bad1f
Gijs Vermeulen <gverm@inuits.eu>
2023-04-04 09:20:17 +0000

    Revert "TEST: make queues not durable"
    This reverts commit 09bc074a6c2d6a454c328e82b5e1262339f42dc4

--- api/amqp/pikamanager.py
+++ api/amqp/pikamanager.py
@@ -1,7 +1,6 @@
 from rabbitmq_pika_flask import RabbitMQ
-from rabbitmq_pika_flask.QueueParams import QueueParams
 
 
 class PikaAmqpManager(RabbitMQ):
     def __init__(self):
-        super().__init__(queue_params=QueueParams(False, False, False))
+        super().__init__()
