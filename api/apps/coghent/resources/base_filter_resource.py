faae244478ad8a749c675504071243dc17e24d4d
Gijs Vermeulen <gverm@inuits.eu>
2023-05-23 11:32:03 +0200

    Remove coghent module

--- api/apps/coghent/resources/base_filter_resource.py
+++
@@ -1,7 +0,0 @@
-from apps.coghent.resources.base_resource import CoghentBaseResource
-from resources.base_filter_resource import BaseFilterResource
-
-
-class CoghentBaseFilterResource(CoghentBaseResource, BaseFilterResource):
-    def __init__(self):
-        super().__init__()
