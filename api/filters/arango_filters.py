0b9694f12761892d68ce59b49b55b324abf5521d
Ricardo Acosta Torres <ricardo@inuits.eu>
2024-01-31 09:45:47 +0100

    Removed logs

--- api/filters/arango_filters.py
+++ api/filters/arango_filters.py
@@ -1,6 +1,5 @@
 from filters.types.filter_types import get_filter
 from storage.arangostore import ArangoStorageManager
-import app
 
 class ArangoFilters(ArangoStorageManager):
     def filter(self, body, skip, limit, collection="entities", order_by=None, asc=True):
@@ -8,55 +7,20 @@ class ArangoFilters(ArangoStorageManager):
             raise ValueError("DB is not initialized")
 
         aql = self.__generate_aql_query(body, collection, order_by, asc)
-        
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 1
-
-        # aql = """
-        #  LET results0 = (
-        #         FOR doc IN entities     
-        #         FILTER (IS_ARRAY(doc.type) AND "asset" IN doc.type)
-        #             OR (doc.type == "asset")
-        #                     RETURN doc
-        # )
-        
-        # FOR result IN results0
-        #     LET sortField = FIRST(
-        #         FOR meta IN result.metadata 
-        #         FILTER meta.key == "title"
-        #         RETURN meta.value
-        #     )
-            
-        #     SORT sortField DESC
-        #     LIMIT @skip, @limit
-        #     RETURN result._key
-        # """
-                
         bind = {"skip": skip, "limit": limit}
-        
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG HARDCODED ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 1
-        app.logger.error(f"{ aql }")
 
         results = self.db.aql.execute(aql, bind_vars=bind, full_count=True) 
         
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG HARDCODED RESULTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 1
-        app.logger.error(f"{ results }")
-
         ids_list = list(results)
         
         filters = {"ids": ids_list}  # type: ignore
 
         id_position_map = {str(doc): index for index, doc in enumerate(ids_list)}
 
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 1.1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 1.1
-
         items = self.get_items_from_collection(collection, 0, limit, None, filters)
 
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 1.2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 1.2
 
         items["results"] = sorted(items["results"], key=lambda x: id_position_map.get(x["_key"]))
-
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 1.3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 1.3
-
         items["count"] = results.statistics()["fullCount"]  # type: ignore
         items["limit"] = limit
 
@@ -74,9 +38,6 @@ class ArangoFilters(ArangoStorageManager):
                         else:
                             options.append(metadata["value"])
             items["results"] = [{"options": options}]
-
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 1.4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 1.4
-
         return items
 
     def __generate_aql_query(
@@ -86,7 +47,6 @@ class ArangoFilters(ArangoStorageManager):
         result_sets = []
         counter = 0
         collection_or_result_set = collection
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 2
 
         for index, filter_criteria in enumerate(filter_request_body):
             
@@ -101,7 +61,6 @@ class ArangoFilters(ArangoStorageManager):
             if filter_criteria.get("operator", "and") != "or" and index > 0:
                 collection_or_result_set = f"results{counter - 1}"
             
-            app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 2.{index+1} ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 2.1, 2.2, ...
 
             aql += f"""
                 LET {result_set} = (
@@ -126,8 +85,6 @@ class ArangoFilters(ArangoStorageManager):
             if len(result_sets) > 1:
                 final_result = f"UNION_DISTINCT({', '.join(result_sets)})"
                 
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 3
-
         aql += f"""
             FOR result IN {final_result if final_result else collection}
                 FILTER HAS(result, 'metadata')
@@ -140,10 +97,5 @@ class ArangoFilters(ArangoStorageManager):
                 LIMIT @skip, @limit
                 RETURN result._key
         """
-
-
-        
-        app.logger.error(f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~ LOG 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")  # Log 4
-        app.logger.error(f"{aql}")
         
         return aql
