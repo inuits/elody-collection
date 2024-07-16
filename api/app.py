9aa6cc2ca18134e523cafca881e0c71d67bd3a6b
Delsin Van Grembergen <delsin@inuits.eu>
2024-07-16 13:57:11 +0200

    feat(cron): Enable background scheduler in .env

--- api/app.py
+++ api/app.py
@@ -41,11 +41,13 @@ def init_app():
     load_apps(app, None)
     return app
 
-def init_scheduler():    
-    checker = TtlChecker()
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(checker, 'cron', hour=12, minute=10) 
-    scheduler.start()
+def init_scheduler():
+    load_background_scheduler = getenv("LOAD_BACKGROUND_SCHEDULER", False)
+    if load_background_scheduler:
+        checker = TtlChecker()
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(checker, 'cron', hour=12, minute=10) 
+        scheduler.start()
 
 def register_swaggerui(app):
     swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
