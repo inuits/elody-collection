15989bb286cec1d3dadab9ba7f0f20ce1ac50890
Eray Özcan <eray@inuits.eu>
2025-02-14 16:45:41 +0100

    fix: undo wrong commit

--- api/object_configurations/none_configuration.py
+++ api/object_configurations/none_configuration.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from elody.object_configurations.base_object_configuration import (
     BaseObjectConfiguration,
 )
@@ -9,12 +8,7 @@ class NoneConfiguration(BaseObjectConfiguration):
     SCHEMA_VERSION = BaseObjectConfiguration.SCHEMA_VERSION
 
     def crud(self):
-        crud = {
-            "nested_matcher_builder": lambda object_lists, keys_info, value, **kwargs: self.__build_nested_matcher(
-                object_lists, keys_info, value, **kwargs
-            )
-        }
-        return {**super().crud(), **crud}
+        return super().crud()
 
     def document_info(self):
         return {"object_lists": {"metadata": "key", "relations": "type"}}
@@ -31,38 +25,3 @@ class NoneConfiguration(BaseObjectConfiguration):
     def validation(self):
         return super().validation()
 
-    def __build_nested_matcher(
-        self, object_lists, keys_info, value, *, index=0, **kwargs
-    ):
-        if index == 0 and not any(info["object_list"] for info in keys_info):
-            if value in ["ANY_MATCH", "NONE_MATCH"]:
-                value = {"$exists": value == "ANY_MATCH"}
-            matcher = {".".join(info["key"] for info in keys_info): value}
-            if inner_exact_matches := kwargs.get("inner_exact_matches"):
-                matcher.update(inner_exact_matches)
-            return matcher
-
-        info = keys_info[index]
-
-        if info["object_list"]:
-            elem_match = {
-                "$elemMatch": {
-                    object_lists[info["object_list"]]: info["object_key"],
-                    **self.__build_nested_matcher(
-                        object_lists, keys_info[index + 1 :], value, index=0, **kwargs
-                    ),
-                }
-            }
-            if value in ["ANY_MATCH", "NONE_MATCH"]:
-                elem_match_with_exists_operator = deepcopy(elem_match)
-                del elem_match["$elemMatch"][keys_info[index + 1]["key"]]
-                if value == "NONE_MATCH":
-                    return {
-                        "NOR_MATCHER": [
-                            {info["key"]: {"$all": [elem_match]}},
-                            {info["key"]: {"$all": [elem_match_with_exists_operator]}},
-                        ]
-                    }
-            return elem_match if index > 0 else {info["key"]: {"$all": [elem_match]}}
-
-        raise Exception(f"Unable to build nested matcher. See keys_info: {keys_info}")
