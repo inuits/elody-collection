aa7846f1f36f2ccca5ce9473a8f3b1ab1b54d818
Tim Standaert <tim.standaert@inuits.eu>
2025-11-19 10:27:17 +0100

    feat: use new regex matcher for `q=key==value` query parameters

--- api/resources/base_resource.py
+++ api/resources/base_resource.py
@@ -473,8 +473,8 @@ class BaseResource(Resource):
                             ),
                             "value": f"^{value}$",  # changing this regex will break pza
                             "match_exact": False,
-                            # "regex": True,
-                            # "regex_options": "i",
+                            "regex": True,
+                            "regex_options": "i",
                         }
                     )
         return filters
