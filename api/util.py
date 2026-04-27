6bcabf8166e6ebb7f0fddd9f32b102d2a9d1b4a1
Tim Standaert <tim.standaert@inuits.eu>
2026-04-27 15:31:13 +0200

    Revert "TEMP: overwrite sdk load_queues function for debugging"
    This reverts commit a14a75a7011ee3f623e14573d05f77f28386f85f.

--- api/util.py
+++ api/util.py
@@ -1,8 +1,3 @@
-import os
-from importlib import import_module
-from inspect import getmembers, isfunction
-
-from elody import util
 from werkzeug.exceptions import BadRequest
 
 
@@ -12,18 +7,3 @@ def encode_content_type_header(content_type: str, fallback_content_type=""):
             raise BadRequest("No Content-Type provided")
         content_type = fallback_content_type
     return content_type.replace("/", "").replace(".", "").replace("-", "")
-
-
-def load_queues(logger):
-    import_module("resources.queues")
-    apps = util.read_json_as_dict(os.getenv("APPS_MANIFEST"), logger)
-    for app in apps:
-        try:
-            module = import_module(f"apps.{app}.resources.queues")
-            logger.info(f"Queues for {app} were loaded.")
-            members = getmembers(module, isfunction)
-            queues = [o for o in members if o[1].__module__ == module.__name__]
-            for item in queues:
-                logger.info(f"Imported {item}")
-        except ModuleNotFoundError:
-            logger.warning(f"Queues for {app} could not be loaded.")
