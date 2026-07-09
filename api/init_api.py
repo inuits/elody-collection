134d38a5d19d7dd41bff37a471d50a5d109cc338
Tim Standaert <tim.standaert@inuits.eu>
2026-07-09 15:02:34 +0200

    chore(cleanup): remove commented code

--- api/init_api.py
+++ api/init_api.py
@@ -2,56 +2,8 @@ from configuration import get_route_mapper
 from flask_restful import Api
 from resources.batch import Batch
 from resources.config import Config
-
-# from resources.entity import (
-#     Entity,
-#     EntityDetail,
-#     EntityMediafiles,
-#     EntityMediafilesCreate,
-#     EntityMetadata,
-#     EntityMetadataKey,
-#     EntityOrder,
-#     EntityRelations,
-#     EntityRelationsAll,
-#     EntitySetPrimaryMediafile,
-#     EntitySetPrimaryThumbnail,
-# )
 from resources.filter import FilterMatchers
-
-# from resources.filter import (
-#     FilterEntities,
-#     FilterEntitiesBySavedSearchId,
-#     FilterEntitiesV2,
-#     FilterGenericObjects,
-#     FilterGenericObjectsBySavedSearchId,
-#     FilterGenericObjectsV2,
-#     FilterMatchers,
-#     FilterMediafiles,
-#     FilterMediafilesBySavedSearchId,
-#     FilterMediafilesV2,
-# )
-# from resources.generic_object import (
-#     GenericObject,
-#     GenericObjectDetail,
-#     GenericObjectDetailV2,
-#     GenericObjectMetadata,
-#     GenericObjectMetadataKey,
-#     GenericObjectRelations,
-#     GenericObjectV2,
-# )
 from resources.history import History
-
-# from resources.mediafile import (
-#     Mediafile,
-#     MediafileAssets,
-#     MediafileCopyright,
-#     MediafileDerivatives,
-#     MediafileDetail,
-#     MediafileDownload,
-#     MediafileMetadata,
-#     MediafileParent,
-#     MediafileRelations,
-# )
 from resources.job import (
     AddDocumentToJob,
     FailJob,
@@ -60,7 +12,6 @@ from resources.job import (
     InitJob,
     StartJob,
 )
-from resources.key_value_store import KeyValueStore, KeyValueStoreDetail
 from resources.saved_search import SavedSearch, SavedSearchDetail
 from resources.share_link import ShareLink, ShareLinkDetail
 from resources.spec import AsyncAPISpec, OpenAPISpec
@@ -80,161 +31,10 @@ def init_api(app):
     api.add_resource(Batch, get_route_mapper().get(Batch.__name__, "/batch"))
     api.add_resource(Config, get_route_mapper().get(Config.__name__, "/config"))
 
-    # api.add_resource(Entity, get_route_mapper().get(Entity.__name__, "/entities"))
-    # api.add_resource(
-    #     EntityDetail,
-    #     get_route_mapper().get(EntityDetail.__name__, "/entities/<string:id>"),
-    # )
-    # api.add_resource(
-    #     EntityMediafiles,
-    #     get_route_mapper().get(
-    #         EntityMediafiles.__name__, "/entities/<string:id>/mediafiles"
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntityMediafilesCreate,
-    #     get_route_mapper().get(
-    #         EntityMediafilesCreate.__name__, "/entities/<string:id>/mediafiles/create"
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntityMetadata,
-    #     get_route_mapper().get(
-    #         EntityMetadata.__name__, "/entities/<string:id>/metadata"
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntityMetadataKey,
-    #     get_route_mapper().get(
-    #         EntityMetadataKey.__name__, "/entities/<string:id>/metadata/<string:key>"
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntityRelations,
-    #     get_route_mapper().get(
-    #         EntityRelations.__name__, "/entities/<string:id>/relations"
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntityRelationsAll,
-    #     get_route_mapper().get(
-    #         EntityRelationsAll.__name__, "/entities/<string:id>/relations/all"
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntitySetPrimaryMediafile,
-    #     get_route_mapper().get(
-    #         EntitySetPrimaryMediafile.__name__,
-    #         "/entities/<string:id>/set_primary_mediafile/<string:mediafile_id>",
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntitySetPrimaryThumbnail,
-    #     get_route_mapper().get(
-    #         EntitySetPrimaryThumbnail.__name__,
-    #         "/entities/<string:id>/set_primary_thumbnail/<string:mediafile_id>",
-    #     ),
-    # )
-    # api.add_resource(
-    #     EntityOrder,
-    #     get_route_mapper().get(
-    #         EntityOrder.__name__,
-    #         "/entities/<string:id>/order",
-    #     ),
-    # )
-
-    # api.add_resource(
-    #     FilterEntities,
-    #     get_route_mapper().get(FilterEntities.__name__, "/entities/filter"),
-    # )
-    # api.add_resource(
-    #     FilterEntitiesBySavedSearchId,
-    #     get_route_mapper().get(
-    #         FilterEntitiesBySavedSearchId.__name__, "/entities/filter/<string:id>"
-    #     ),
-    # )
-    # api.add_resource(
-    #     FilterEntitiesV2,
-    #     get_route_mapper().get(FilterEntitiesV2.__name__, "/entities/filter_v2"),
-    # )
-    # api.add_resource(
-    #     FilterGenericObjects,
-    #     get_route_mapper().get(
-    #         FilterGenericObjects.__name__, "/<string:collection>/filter"
-    #     ),
-    # )
-    # api.add_resource(
-    #     FilterGenericObjectsBySavedSearchId,
-    #     get_route_mapper().get(
-    #         FilterGenericObjectsBySavedSearchId.__name__,
-    #         "/<string:collection>/filter/<string:id>",
-    #     ),
-    # )
-    # api.add_resource(
-    #     FilterGenericObjectsV2,
-    #     get_route_mapper().get(
-    #         FilterGenericObjectsV2.__name__, "/<string:collection>/filter_v2"
-    #     ),
-    # )
     api.add_resource(
         FilterMatchers,
         get_route_mapper().get(FilterMatchers.__name__, "/filter/matchers"),
     )
-    # api.add_resource(
-    #     FilterMediafiles,
-    #     get_route_mapper().get(FilterMediafiles.__name__, "/mediafiles/filter"),
-    # )
-    # api.add_resource(
-    #     FilterMediafilesBySavedSearchId,
-    #     get_route_mapper().get(
-    #         FilterMediafilesBySavedSearchId.__name__, "/mediafiles/filter/<string:id>"
-    #     ),
-    # )
-    # api.add_resource(
-    #     FilterMediafilesV2,
-    #     get_route_mapper().get(FilterMediafilesV2.__name__, "/mediafiles/filter_v2"),
-    # )
-
-    # api.add_resource(
-    #     GenericObject,
-    #     get_route_mapper().get(GenericObject.__name__, "/<string:collection>"),
-    # )
-    # api.add_resource(
-    #     GenericObjectDetail,
-    #     get_route_mapper().get(
-    #         GenericObjectDetail.__name__, "/<string:collection>/<string:id>"
-    #     ),
-    # )
-    # api.add_resource(
-    #     GenericObjectDetailV2,
-    #     get_route_mapper().get(
-    #         GenericObjectDetailV2.__name__, "/v2/<string:collection>/<string:id>"
-    #     ),
-    # )
-    # api.add_resource(
-    #     GenericObjectMetadata,
-    #     get_route_mapper().get(
-    #         GenericObjectMetadata.__name__, "/<string:collection>/<string:id>/metadata"
-    #     ),
-    # )
-    # api.add_resource(
-    #     GenericObjectMetadataKey,
-    #     get_route_mapper().get(
-    #         GenericObjectMetadataKey.__name__,
-    #         "/<string:collection>/<string:id>/metadata/<string:key>",
-    #     ),
-    # )
-    # api.add_resource(
-    #     GenericObjectRelations,
-    #     get_route_mapper().get(
-    #         GenericObjectRelations.__name__,
-    #         "/<string:collection>/<string:id>/relations",
-    #     ),
-    # )
-    # api.add_resource(
-    #     GenericObjectV2,
-    #     get_route_mapper().get(GenericObjectV2.__name__, "/v2/<string:collection>"),
-    # )
 
     api.add_resource(
         History,
@@ -242,67 +42,6 @@ def init_api(app):
             History.__name__, "/history/<string:collection>/<string:id>"
         ),
     )
-
-    api.add_resource(
-        KeyValueStore,
-        get_route_mapper().get(KeyValueStore.__name__, "/key_value_store"),
-    )
-    api.add_resource(
-        KeyValueStoreDetail,
-        get_route_mapper().get(
-            KeyValueStoreDetail.__name__, "/key_value_store/<string:id>"
-        ),
-    )
-
-    # api.add_resource(
-    #     Mediafile, get_route_mapper().get(Mediafile.__name__, "/mediafiles")
-    # )
-    # api.add_resource(
-    #     MediafileAssets,
-    #     get_route_mapper().get(
-    #         MediafileAssets.__name__, "/mediafiles/<string:id>/assets"
-    #     ),
-    # )
-    # api.add_resource(
-    #     MediafileCopyright,
-    #     get_route_mapper().get(
-    #         MediafileCopyright.__name__, "/mediafiles/<string:id>/copyright"
-    #     ),
-    # )
-    # api.add_resource(
-    #     MediafileDerivatives,
-    #     get_route_mapper().get(
-    #         MediafileDerivatives.__name__, "/mediafiles/<string:id>/derivatives"
-    #     ),
-    # )
-    # api.add_resource(
-    #     MediafileDownload,
-    #     get_route_mapper().get(
-    #         MediafileDownload.__name__, "/mediafiles/<string:id>/download"
-    #     ),
-    # )
-    # api.add_resource(
-    #     MediafileDetail,
-    #     get_route_mapper().get(MediafileDetail.__name__, "/mediafiles/<string:id>"),
-    # )
-    # api.add_resource(
-    #     MediafileMetadata,
-    #     get_route_mapper().get(
-    #         MediafileMetadata.__name__, "/mediafiles/<string:id>/metadata"
-    #     ),
-    # )
-    # api.add_resource(
-    #     MediafileParent,
-    #     get_route_mapper().get(
-    #         MediafileParent.__name__, "/mediafiles/<string:id>/parent"
-    #     ),
-    # )
-    # api.add_resource(
-    #     MediafileRelations,
-    #     get_route_mapper().get(
-    #         MediafileRelations.__name__, "/mediafiles/<string:id>/relations"
-    #     ),
-    # )
 
     api.add_resource(
         OpenAPISpec,
