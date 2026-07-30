ccc2716db15fbb91ade852e6f7a091fc708e07b5
Eray Özcan <eray@inuits.eu>
2026-07-30 11:07:43 +0200

    fix: do not sync relations with type None
    refs #157876
    
    (cherry picked from commit 0f4ba2bbf32b3d2021e59d8d66f5ffa203e1a62d)

--- api/storage/mongostore.py
+++ api/storage/mongostore.py
@@ -66,6 +66,8 @@ class MongoStorageManager(GenericStorageManager):
             )
             dst_relation = relation.copy()
             dst_relation["type"] = self._map_entity_relation(relation["type"])
+            if not dst_relation.get("type"):
+                continue
             dst_relation["key"] = id
             dst_id = relation["key"]
             dst_content = [dst_relation]
@@ -379,6 +381,7 @@ class MongoStorageManager(GenericStorageManager):
     def add_relations_to_collection_item(
         self, collection, id, relations, parent=True, dst_collection=None
     ):
+        relations = [relation for relation in relations if relation.get("type")]
         self.add_sub_item_to_collection_item(collection, id, "relations", relations)
         self.__add_child_relations(id, relations, dst_collection)
         return relations
